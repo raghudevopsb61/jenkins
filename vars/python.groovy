f43f4ad7bcdf3bbc1be5e1d1f0ffd31e6157186f
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2021-12-30 06:41:49 +0530

    Update jenkins/

--- vars/python.groovy
+++ vars/python.groovy
@@ -12,7 +12,9 @@ def call() {
 
       stage('Check the Code Quality') {
         steps {
-          sh 'echo Check the code Quality'
+          script {
+            common.sonarQube()
+          }
         }
       }
 
