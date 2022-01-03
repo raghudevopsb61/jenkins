dca34ca221702a7647629899a592284503165735
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-01-03 06:27:30 +0530

    Update jenkins/

--- vars/common.groovy
+++ vars/common.groovy
@@ -1,3 +1,6 @@
+import org.jenkinsci.plugins.pipeline.modeldefinition.Utils
+
+
 def sonarQube() {
 //  sh 'sonar-scanner -Dsonar.projectKey=${COMPONENT} -Dsonar.sources=. -Dsonar.host.url=http://172.31.15.251:9000 -Dsonar.login=admin -Dsonar.password=admin123'
 //  sh 'sonar-quality-gate.sh admin admin123 172.31.15.251 ${COMPONENT}'
