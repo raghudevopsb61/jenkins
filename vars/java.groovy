3a7d638c3453c980512102ad0af6486520edc397
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-01-06 06:09:29 +0530

    Update jenkins/

--- vars/java.groovy
+++ vars/java.groovy
@@ -42,6 +42,18 @@ def call() {
         }
       }
 
+      stage('Publish Artifacts') {
+        when {
+          expression { sh([returnStdout: true, script: 'echo ${GIT_BRANCH} | grep tags || true' ]) }
+        }
+        steps {
+          script {
+            common.prepareArtifacts()
+            common.publishArtifacts()
+          }
+        }
+      }
+
     }
 
     post {
