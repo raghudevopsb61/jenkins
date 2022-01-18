c6dcb9c308e448393badad4a65bc074fe80ae150
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-01-18 07:20:39 +0530

    Update jenkins/

--- seed.groovy
+++ seed.groovy
@@ -120,3 +120,23 @@ pipelineJob('Mutable/APP-SETUP') {
   }
 }
 
+pipelineJob('Mutable/VPC') {
+  configure { flowdefinition ->
+    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+        'userRemoteConfigs' {
+          'hudson.plugins.git.UserRemoteConfig' {
+            'url'('https://github.com/raghudevopsb61/terraform-mutable-alb.git')
+          }
+        }
+        'branches' {
+          'hudson.plugins.git.BranchSpec' {
+            'name'('*/main')
+          }
+        }
+      }
+      'scriptPath'('Jenkinsfile')
+      'lightweight'(true)
+    }
+  }
+}
