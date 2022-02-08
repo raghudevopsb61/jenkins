c1ac206937300729e5cd40de1704944564cc6062
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-02-08 07:19:00 +0530

    Update jenkins/

--- instance-refresh.sh
+++ instance-refresh.sh
@@ -6,6 +6,7 @@ aws autoscaling start-instance-refresh --auto-scaling-group-name ${ASG}
 while true ; do
   STATUS=$(aws autoscaling describe-instance-refreshes --auto-scaling-group-name ${ASG} | jq .InstanceRefreshes[0].Status | sed -e 's/"//g')
   if [ "${STATUS}" == "InProgress" -o "${STATUS}" == "Pending" ]; then
+    eco Instance Refresh Status - ${STATUS}
     sleep 30
   elif [ "${STATUS}" == "Successful" ]; then
     exit 0
