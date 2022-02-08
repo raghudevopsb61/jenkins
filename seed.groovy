d501f4130b11ecf6ed64a501d5b62a73871520b6
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-02-08 06:44:09 +0530

    Update jenkins/

--- seed.groovy
+++ seed.groovy
@@ -257,3 +257,23 @@ pipelineJob('Immutable/All-Infra-Destroy') {
   }
 }
 
+pipelineJob('Immutable/App-Deploy') {
+  configure { flowdefinition ->
+    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+        'userRemoteConfigs' {
+          'hudson.plugins.git.UserRemoteConfig' {
+            'url'('https://github.com/raghudevopsb61/jenkins.git')
+          }
+        }
+        'branches' {
+          'hudson.plugins.git.BranchSpec' {
+            'name'('*/main')
+          }
+        }
+      }
+      'scriptPath'('Jenkinsfile-Immutable-App-Deploy')
+      'lightweight'(true)
+    }
+  }
+}
